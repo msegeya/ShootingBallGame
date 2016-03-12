2b1e848d645a580525a01eaf64e0fb9f44eda314
Pengfei Xing <novelidea1992@gmail.com>
2016-03-12 15:10:37 -0800

    shooting ball

--- ShootingBallGame/ShootingBallGame/GameScene.swift
+++ ShootingBallGame/ShootingBallGame/GameScene.swift
@@ -27,6 +27,20 @@ class GameScene: SKScene {
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         touchLocation = touches.first!.locationInNode(self)
     }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+
+        let ball : SKSpriteNode = SKScene(fileNamed:"Ball")!.childNodeWithName("ball")! as! SKSpriteNode
+        ball.removeFromParent()
+        self.addChild(ball)
+        ball.zPosition = 0
+        ball.position = cannon.position
+        let angleInRadians = Float(cannon.zRotation)
+        let speed = CGFloat(105.0)
+        let vx : CGFloat = CGFloat(cosf(angleInRadians)) * speed
+        let vy : CGFloat = CGFloat(sinf(angleInRadians)) * speed
+        ball.physicsBody?.applyImpulse(CGVectorMake(vx, vy))
+    }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
