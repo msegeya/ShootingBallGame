18616aa9b38980635267b42ad464d50afb885f77
Pengfei Xing <novelidea1992@gmail.com>
2016-03-13 11:08:27 -0700

    add light effect

--- ShootingBallGame/ShootingBallGame/GameScene.swift
+++ ShootingBallGame/ShootingBallGame/GameScene.swift
@@ -7,6 +7,7 @@
 //
 
 import SpriteKit
+import AVFoundation
 
 let wallMask:UInt32 = 0x1 << 0 // 1
 let ballMask:UInt32 = 0x1 << 1 // 2
@@ -16,12 +17,56 @@ let orangePegMask:UInt32 = 0x1 << 4 // 16
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     var cannon:SKSpriteNode!
+    var block:SKSpriteNode!
+//    var bucket:SKSpriteNode!
     var touchLocation:CGPoint = CGPointZero
+    var background : SKAudioNode!
+    
+    var light : SKLightNode!
+    var nodeToFollow : SKNode!
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         cannon = self.childNodeWithName("cannon") as! SKSpriteNode
+        block = self.childNodeWithName("block") as! SKSpriteNode
+        light = self.childNodeWithName("light") as! SKLightNode
+//        bucket = self.childNodeWithName("bucket") as! SKSpriteNode
         self.physicsWorld.contactDelegate = self
+        
+        let degrees = 90.0
+        let rads = degrees * M_PI / 180
+        let action = SKAction.rotateByAngle(CGFloat(rads), duration: 3)
+//        block.runAction(action)
+        block.runAction(SKAction.repeatActionForever(action))
+        
+//        let a1 = SKAction.moveByX(840.0, y: 0.0, duration: 3.0)
+//        a1.timingMode = .EaseInEaseOut
+//        let a2 = SKAction.moveByX(-840.0, y: 0.0, duration: 3.0)
+//        a2.timingMode = .EaseInEaseOut
+//        let seq = SKAction.sequence([a1, a2])
+//        bucket.runAction(SKAction.repeatActionForever(seq))
+        
+//        background = SKAudioNode(fileNamed : "bg.mp3")
+//        self.addChild(background)
+        do {
+            let sounds = ["cannon", "hit"]
+            for sound in sounds {
+                let player = try AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(sound, ofType: "wav")!))
+                player.prepareToPlay()
+            }
+        } catch {
+            
+        }
+    }
+    
+    func createFrameAnimation() {
+        var frames : [SKTexture] = []
+        for var i : Int = 1; i <= 10; i++ {
+            let tex : SKTexture  = SKTexture(imageNamed: "\(i).png")
+            frames.append(tex)
+        }
+        //run animation
+        //object.runAction(SKAction.animateWithTextures(frames, timePerFrame:0.03))
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -45,6 +90,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         ball.physicsBody?.applyImpulse(CGVectorMake(vx, vy))
         ball.physicsBody?.collisionBitMask = wallMask | ballMask | pegMask | orangePegMask | squareMask
         ball.physicsBody?.contactTestBitMask = ball.physicsBody!.collisionBitMask
+        
+        self.runAction(SKAction.playSoundFileNamed("cannon.wav", waitForCompletion: true))
+//        background.runAction(SKAction.stop())
+        nodeToFollow = ball
     }
     
     override func update(currentTime: CFTimeInterval) {
@@ -52,6 +101,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let percent = touchLocation.x / size.width
         let newAngle = percent * 180 - 180
         cannon.zRotation = CGFloat(newAngle) * CGFloat(M_PI) / 180.0
+        
+        if nodeToFollow != nil {
+            light.position = nodeToFollow.position
+        }
     }
     
     func didBeginContact(contact: SKPhysicsContact) {
@@ -61,13 +114,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             self.didHitPeg(other)
         }
         else if other.categoryBitMask == squareMask {
-            print("hit square!")
+//            print("hit square!")
         }
         else if other.categoryBitMask == wallMask {
-            print("hit wall!")
+//            print("hit wall!")
         }
         else if other.categoryBitMask == ballMask {
-            print("hit ball!")
+//            print("hit ball!")
         }
     }
     
@@ -80,5 +133,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         spark.particleColor = (peg.categoryBitMask == orangePegMask) ? orange : blue
         self.addChild(spark)
         peg.node?.removeFromParent()
+        
+        self.runAction(SKAction.playSoundFileNamed("hit.wav", waitForCompletion: true))
     }
 }
